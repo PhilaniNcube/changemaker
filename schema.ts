607ce57fa1cd065c66f3a6e5e18d5243fb881e07
Philani Ncube <ncbphi001@gmail.com>
2023-02-28 07:43:08 +0200

    schema update

--- schema.ts
+++ schema.ts
@@ -9,6 +9,64 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      districts: {
+        Row: {
+          id: string
+          created_at: string | null
+          name: string | null
+          slug: string | null
+          province_id: string | null
+        }
+        Insert: {
+          id?: string
+          created_at?: string | null
+          name?: string | null
+          slug?: string | null
+          province_id?: string | null
+        }
+        Update: {
+          id?: string
+          created_at?: string | null
+          name?: string | null
+          slug?: string | null
+          province_id?: string | null
+        }
+      }
+      organisations: {
+        Row: {
+          id: string
+          created_at: string | null
+          name: string | null
+          facebook: string | null
+          twitter: string | null
+          website: string | null
+          description: string | null
+          district_id: string | null
+          province_id: string | null
+        }
+        Insert: {
+          id?: string
+          created_at?: string | null
+          name?: string | null
+          facebook?: string | null
+          twitter?: string | null
+          website?: string | null
+          description?: string | null
+          district_id?: string | null
+          province_id?: string | null
+        }
+        Update: {
+          id?: string
+          created_at?: string | null
+          name?: string | null
+          facebook?: string | null
+          twitter?: string | null
+          website?: string | null
+          description?: string | null
+          district_id?: string | null
+          province_id?: string | null
+        }
+      }
       profiles: {
         Row: {
           id: string
@@ -24,6 +82,61 @@ export interface Database {
           id?: string
           first_name?: string | null
           last_name?: string | null
+        }
+      }
+      provinces: {
+        Row: {
+          id: string
+          created_at: string | null
+          name: string | null
+          slug: string | null
+        }
+        Insert: {
+          id?: string
+          created_at?: string | null
+          name?: string | null
+          slug?: string | null
+        }
+        Update: {
+          id?: string
+          created_at?: string | null
+          name?: string | null
+          slug?: string | null
+        }
+      }
+      workshops: {
+        Row: {
+          id: string
+          created_at: string | null
+          name: string | null
+          slug: string | null
+          organisation_id: string | null
+          date: string | null
+          time: string | null
+          description: string | null
+          image: string | null
+        }
+        Insert: {
+          id?: string
+          created_at?: string | null
+          name?: string | null
+          slug?: string | null
+          organisation_id?: string | null
+          date?: string | null
+          time?: string | null
+          description?: string | null
+          image?: string | null
+        }
+        Update: {
+          id?: string
+          created_at?: string | null
+          name?: string | null
+          slug?: string | null
+          organisation_id?: string | null
+          date?: string | null
+          time?: string | null
+          description?: string | null
+          image?: string | null
         }
       }
     }
