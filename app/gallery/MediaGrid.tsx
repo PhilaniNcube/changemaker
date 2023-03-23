e437ab0ffe243ea861bd8c00655bfe6f3fa34df6
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2023-03-23 16:11:41 +0200

    Image link

--- app/gallery/MediaGrid.tsx
+++ app/gallery/MediaGrid.tsx
@@ -15,7 +15,7 @@ const MediaGrid = ({media}:Props) => {
       {" "}
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {media.map((image) => (
-          <div
+          <Link href={image.src}
             key={image.id}
             className="w-full relative aspect-video rounded-md overflow-hidden shadow-md"
           >
@@ -23,7 +23,7 @@ const MediaGrid = ({media}:Props) => {
             <section>
               <article>
                 <div>
-                  <Link href={image.src} >
+
                     <Image
                       src={image.src}
                       width={image.width!}
@@ -31,7 +31,7 @@ const MediaGrid = ({media}:Props) => {
                       alt={image.filename!}
                       className="w-full aspect-video rounded-md object-cover"
                     />
-                  </Link>
+
                   <div className="bg-gradient-to-t from-slate-600/50 to-transparent absolute inset-0 flex items-end justify-start p-5">
                     <div>
                       <p className="text-md text-white">{image.filename}</p>
@@ -43,7 +43,7 @@ const MediaGrid = ({media}:Props) => {
                 </div>
               </article>
             </section>
-          </div>
+          </Link>
         ))}
       </div>{" "}
     </section>
