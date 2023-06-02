7b55041bb4bceff759dfb54e33b88e19c1503dfc
Philani Ncube <ncbphi001@gmail.com>
2023-06-02 12:49:49 +0200

    facebook on learner area

--- app/learner-area/page.tsx
+++ app/learner-area/page.tsx
@@ -2,13 +2,25 @@ import { Fragment } from "react";
 import Hero from "./Hero";
 import Carousel from "./Carousel";
 import Benefits from "./Benefits";
+import Script from "next/script";
 
 const page = () => {
-  return <main className="bg-white">
-    {/* <Hero /> */}
-    <Benefits />
-    <Carousel />
-
-  </main>;
+  return (
+    <main className="bg-white">
+      {/* <Hero /> */}
+      <Benefits />
+      <Carousel />
+      <Fragment>
+        <Script
+          async
+          defer
+          crossOrigin="anonymous"
+          src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v16.0"
+          nonce="i0LiW14P"
+        />
+        <div id="fb-root"></div>
+      </Fragment>
+    </main>
+  );
 };
 export default page;
