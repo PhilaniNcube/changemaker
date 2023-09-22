f1132ed221b414f9353ee7cc02bddb32f7e646e6
Philani Ncube <ncbphi001@gmail.com>
2023-09-22 17:11:17 +0200

    national video

--- app/national/NationalVideo.tsx
+++ app/national/NationalVideo.tsx
@@ -2,15 +2,17 @@
 
 const NationalVideo = () => {
   return (
-    <section className="bg-white px-8 md:px-6 py-12 mt-6" id="video">
+    <section className="px-8 py-12 mt-6 bg-white md:px-6" id="video">
       <iframe
-        className="max-w-4xl aspect-video mx-auto"
+        className="max-w-4xl mx-auto aspect-video"
         width="900"
-        src="https://www.youtube.com/watch?v=d_uSXPua3SY"
-        title="masifunde CHANGEMAKER NETWORK in Nelson Mandela Bay"
+        src="https://www.youtube.com/embed/d_uSXPua3SY"
+        title="masifunde Changemaker Network (national NISSP version)"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
     </section>
   );
 };
 export default NationalVideo;
+
+
