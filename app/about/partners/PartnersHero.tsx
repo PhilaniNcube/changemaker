12cbde715bd3ca9a105525fe2ad5e8bb3a13f654
Philani Ncube <ncbphi001@gmail.com>
2024-02-13 13:31:18 +0200

    Update PartnersHero component with new image and dimensions

--- app/about/partners/PartnersHero.tsx
+++ app/about/partners/PartnersHero.tsx
@@ -2,16 +2,16 @@ import Image from 'next/image'
 
 const PartnersHero = () => {
   return (
-    <section id="partners" className="relative bg-masifunde isolate">
+    <section id="partners" className="relative py-8 bg-masifunde isolate">
       <Image
-        src="/images/four.jpg"
-        width={2976}
-        height={1984}
+        src="/images/cso.jpg"
+        width={2000}
+        height={1600}
         alt="Team"
-        className="w-full min-h-[55vh] max-h-[70vh] lg:max-h-[56vh] lg:aspect-video object-cover"
+        className="w-full min-h-[55vh] aspect-video object-cover"
       />
       <div className="absolute inset-0 py-10 bg-slate-600/60 ">
-        <div className="flex flex-col items-start justify-center gap-8 px-8 mx-auto text-white lg:h-full min-h-fit max-w-7xl">
+        <div className="flex flex-col items-start justify-center h-full gap-8 px-8 mx-auto text-white max-w-7xl">
           <h2 className="font-extrabold text-3xl lg:text-5xl max-w-[32ch] text-center mx-auto">
             Partner CSO&apos;s Profiles
           </h2>
