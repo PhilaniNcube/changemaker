ba2152636ae0a2e96b24ad7c36552b4f99ac904f
Philani Ncube <ncbphi001@gmail.com>
2024-06-06 21:03:00 +0200

    chore: Update styled-components to version 6

--- app/dashboard/media/MediaForm.tsx
+++ app/dashboard/media/MediaForm.tsx
@@ -1,253 +1,266 @@
 "use client";
 import { useState } from "react";
-import { Database } from "@/schema";
-import { SubmitHandler, useForm } from "react-hook-form";
+import type { Database } from "@/schema";
+import { type SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useSupabase } from "@/components/Auth/SupabaseProvider";
-import { CldUploadButton } from "next-cloudinary";
+import { CldUploadButton, type CldUploadWidgetResults } from "next-cloudinary";
 import { useRouter } from "next/navigation";
-import { Folder } from "../page";
+import type { Folder } from "../page";
 
 const mediaSchema = z.object({
-  organisation_id: z.string().uuid(),
-  filename: z.string().min(3, {message: "Please enter a name for your image"}),
-  folder: z.string()
+	organisation_id: z.string().uuid(),
+	filename: z
+		.string()
+		.min(3, { message: "Please enter a name for your image" }),
+	folder: z.string(),
 });
 
 type Media = z.infer<typeof mediaSchema>;
 
 type Info = {
-  access_mode: string;
-  asset_id: string;
-  batchId: string;
-  bytes: number;
-  created_at: string;
-  etag: string;
-  folder: string;
-  format: string;
-  height: number;
-  id: string;
-  original_filename: string;
-  path: string;
-  placeholder: false;
-  public_id: string;
-  resource_type: string;
-  secure_url: string;
-  signature: string;
-  tags: any[];
-  thumbnail_url: string;
-  type: string;
-  url: string;
-  version: number;
-  version_id: string;
-  width: number;
+	access_mode: string;
+	asset_id: string;
+	batchId: string;
+	bytes: number;
+	created_at: string;
+	etag: string;
+	folder: string;
+	format: string;
+	height: number;
+	id: string;
+	original_filename: string;
+	path: string;
+	placeholder: false;
+	public_id: string;
+	resource_type: string;
+	secure_url: string;
+	signature: string;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	tags: any[];
+	thumbnail_url: string;
+	type: string;
+	url: string;
+	version: number;
+	version_id: string;
+	width: number;
 };
 
 type Result = {
-  event: string
-  info: Info
-}
-
-type Props = {
-  organisations: Database["public"]["Tables"]["organisations"]["Row"][];
-  folders: Folder[]
+	event: string | undefined;
+	info: Info;
 };
 
-const MediaForm = ({  organisations, folders }: Props) => {
-  const router = useRouter();
-  const [image, setImage] = useState<Info | null>(null);
-  const [folder, setFolder] = useState("");
+type Props = {
+	organisations: Database["public"]["Tables"]["organisations"]["Row"][];
+	folders: Folder[];
+};
 
-  const {
-    register,
-    handleSubmit,
-    formState: {errors}
-  } = useForm<Media>({
-    resolver: zodResolver(mediaSchema),
-  });
+const MediaForm = ({ organisations, folders }: Props) => {
+	const router = useRouter();
+	const [image, setImage] = useState<Info | null>(null);
+	const [folder, setFolder] = useState("");
 
-  const { supabase } = useSupabase();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Media>({
+		resolver: zodResolver(mediaSchema),
+	});
 
+	const { supabase } = useSupabase();
 
-  const onSubmit: SubmitHandler<Media> = async (data) => {
+	const onSubmit: SubmitHandler<Media> = async (data) => {
+		if (image === null) {
+			alert("Please upload a new image");
+			return;
+		}
 
+		if (data.organisation_id === null) {
+			alert("Please select organisation");
+			return;
+		}
 
-    if(image === null) {
-      alert('Please upload a new image');
-      return
-    } else if (data.organisation_id === null) {
-      alert('Please select organisation');
-      return
-    } else if(!data.filename ) {
-      alert('Please give your image a name');
-      return
-    } else if(!data.folder) {
-      alert('Please select a folder');
-      return
-    }
-      toast("Please wait...", {
-        type: "warning",
-      });
+		if (!data.filename) {
+			alert("Please give your image a name");
+			return;
+		}
 
-const { data: mediaData, error } = await supabase
-  .from("media")
-  .insert([
-    {
-      src: image?.url,
-      height: image?.height!,
-      width: image?.width!,
-      organisation_id: data.organisation_id,
-      filename: data.filename,
-      type: image?.format,
-      public_id: image?.public_id,
-      thumbnail_url: image?.thumbnail_url,
-      folder: data.folder.toLowerCase(),
-    },
-  ]).select('*').single();
+		if (!data.folder) {
+			alert("Please select a folder");
+			return;
+		}
 
-  if(error) {
-    toast(`Could not save media: ${error.message}`,{
-      type: "error"
-    } )
-    return
-  } else if (mediaData) {
-        toast(`Media was saved to database`, {
-          type: "success",
+		toast("Please wait...", {
+			type: "warning",
+		});
 
-        });
+		const { data: mediaData, error } = await supabase
+			.from("media")
+			.insert([
+				{
+					src: image?.url,
+					height: image?.height || 100,
+					width: image?.width || 100,
+					organisation_id: data.organisation_id,
+					filename: data.filename,
+					type: image?.format,
+					public_id: image?.public_id,
+					thumbnail_url: image?.thumbnail_url,
+					folder: data.folder.toLowerCase(),
+				},
+			])
+			.select("*")
+			.single();
 
-        router.push('/gallery')
-  }
+		if (error) {
+			toast(`Could not save media: ${error.message}`, {
+				type: "error",
+			});
+			return;
+		}
 
+		if (mediaData) {
+			toast("Media was saved to database", {
+				type: "success",
+			});
 
-  };
+			router.push("/gallery");
+		}
+	};
 
-  async function handleOnUpload(result: Result, widget: any) {
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+async function handleOnUpload(result: CldUploadWidgetResults, widget: any) {
+if (result.event === "success") {
+	if (result.info && typeof result.info === "object") {
+		setImage(result.info as Info);
+	} else {
+		setImage(null);
+	}
+	widget.close();
+	return;
+}
 
-    if (result.event === "success") {
-      setImage(result.info);
-      widget.close();
-      return;
-    } else if (result.event !== "success") {
-      toast("Image upload failed", {
-        type: "error",
-      });
-      widget.close();
-    }
-  }
+if (result.event !== "success") {
+	toast("Image upload failed", {
+		type: "error",
+	});
+}
+}
 
-  return (
-    <div className="max-w-2xl px-6 pt-10 mt-6 rounded-md shadow-md bg-slate-100">
-      {" "}
-      <ToastContainer position="top-right" autoClose={2500} />
-      <form className="w-full " onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-4 ">
-          <div className="col-span-3 ">
-            <label
-              htmlFor="filename"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Image Name
-            </label>
-            <div className="flex mt-2 rounded-md shadow-sm">
-              <input
-                type="text"
-                id="filename"
-                {...register("filename")}
-                className="block w-full flex-1 rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-masifunde sm:text-sm sm:leading-6"
-              />
-            </div>
-            {errors.filename && (
-              <p className="mt-2 text-xs italic text-red-500">
-                {errors.filename?.message}
-              </p>
-            )}
-          </div>
+	return (
+		<div className="max-w-2xl px-6 pt-10 mt-6 rounded-md shadow-md bg-slate-100">
+			{" "}
+			<ToastContainer position="top-right" autoClose={2500} />
+			<form className="w-full " onSubmit={handleSubmit(onSubmit)}>
+				<div className="grid grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-4 ">
+					<div className="col-span-3 ">
+						<label
+							htmlFor="filename"
+							className="block text-sm font-medium leading-6 text-gray-900"
+						>
+							Image Name
+						</label>
+						<div className="flex mt-2 rounded-md shadow-sm">
+							<input
+								type="text"
+								id="filename"
+								{...register("filename")}
+								className="block w-full flex-1 rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-masifunde sm:text-sm sm:leading-6"
+							/>
+						</div>
+						{errors.filename && (
+							<p className="mt-2 text-xs italic text-red-500">
+								{errors.filename?.message}
+							</p>
+						)}
+					</div>
 
-          <fieldset className="col-span-3 mt-6 ">
-            <legend className="font-medium leading-6 text-gray-700 contents text-md">
-              Select Folder
-            </legend>
+					<fieldset className="col-span-3 mt-6 ">
+						<legend className="font-medium leading-6 text-gray-700 contents text-md">
+							Select Folder
+						</legend>
 
-            <div className="grid grid-cols-2 gap-4 mt-4 md:grid-cols-3">
-              {folders.map((folder, i) => (
-                <div key={i} className="flex items-center">
-                  <input
-                    id="folder"
-                    {...register("folder")}
-                    value={folder.name}
-                    onChange={(e) => setFolder(e.target.value)}
-                    type="radio"
-                    className="w-4 h-4 border-gray-300 text-masifunde focus:ring-masifunde"
-                  />
-                  <label
-                    htmlFor="folder"
-                    className="block ml-3 text-sm font-medium leading-6 text-gray-900 capitalize"
-                  >
-                    {folder.name}
-                  </label>
-                </div>
-              ))}
-            </div>
-          </fieldset>
+						<div className="grid grid-cols-2 gap-4 mt-4 md:grid-cols-3">
+							{folders.map((folder, i) => (
+								<div key={folder.name} className="flex items-center">
+									<input
+										id="folder"
+										{...register("folder")}
+										value={folder.name}
+										onChange={(e) => setFolder(e.target.value)}
+										type="radio"
+										className="w-4 h-4 border-gray-300 text-masifunde focus:ring-masifunde"
+									/>
+									<label
+										htmlFor="folder"
+										className="block ml-3 text-sm font-medium leading-6 text-gray-900 capitalize"
+									>
+										{folder.name}
+									</label>
+								</div>
+							))}
+						</div>
+					</fieldset>
 
-          <div className="col-span-3 sm:col-span-2">
-            <label className="block text-sm font-medium leading-6 text-gray-900 sr-only">
-              Select Image
-            </label>
-            <div className="flex mt-2 rounded-md shadow-sm">
-              <div className="px-4 py-2 text-sm font-medium text-white bg-blue-400 rounded-md w-fit">
-                <CldUploadButton
-                  onUpload={handleOnUpload}
-                  uploadPreset="kfo3j4ot"
-                  options={{
-                    folder: folder,
-                  }}
-                >
-                  Select Image
-                </CldUploadButton>
-              </div>
-            </div>
-          </div>
-        </div>
-        <fieldset className="mt-6">
-          <legend className="font-medium leading-6 text-gray-700 contents text-md">
-            Select Organisation
-          </legend>
+					<div className="col-span-3 sm:col-span-2">
+						<label className="block text-sm font-medium leading-6 text-gray-900 sr-only">
+							Select Image
+						</label>
+						<div className="flex mt-2 rounded-md shadow-sm">
+							<div className="px-4 py-2 text-sm font-medium text-white bg-blue-400 rounded-md w-fit">
+								<CldUploadButton
+									onUpload={handleOnUpload}
+									uploadPreset="kfo3j4ot"
+									options={{
+										folder: folder,
+									}}
+								>
+									Select Image
+								</CldUploadButton>
+							</div>
+						</div>
+					</div>
+				</div>
+				<fieldset className="mt-6">
+					<legend className="font-medium leading-6 text-gray-700 contents text-md">
+						Select Organisation
+					</legend>
 
-          <div className="grid grid-cols-2 gap-4 mt-4 md:grid-cols-3">
-            {organisations.map((organisation) => (
-              <div key={organisation.id} className="flex items-center">
-                <input
-                  id="organisation_id"
-                  {...register("organisation_id")}
-                  value={organisation.id}
-                  type="radio"
-                  className="w-4 h-4 border-gray-300 text-masifunde focus:ring-masifunde"
-                />
-                <label
-                  htmlFor="organisation_id"
-                  className="block ml-3 text-sm font-medium leading-6 text-gray-900"
-                >
-                  {organisation.name}
-                </label>
-              </div>
-            ))}
-          </div>
-        </fieldset>
-        <div className="px-4 py-3 mt-6 text-right bg-gray-100 sm:px-6">
-          <button
-            type="submit"
-            className="inline-flex justify-center px-10 py-2 text-xl font-semibold text-white rounded-md shadow-sm bg-masifunde hover:bg-masifunde focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-masifunde"
-          >
-            Save
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+					<div className="grid grid-cols-2 gap-4 mt-4 md:grid-cols-3">
+						{organisations.map((organisation) => (
+							<div key={organisation.id} className="flex items-center">
+								<input
+									id="organisation_id"
+									{...register("organisation_id")}
+									value={organisation.id}
+									type="radio"
+									className="w-4 h-4 border-gray-300 text-masifunde focus:ring-masifunde"
+								/>
+								<label
+									htmlFor="organisation_id"
+									className="block ml-3 text-sm font-medium leading-6 text-gray-900"
+								>
+									{organisation.name}
+								</label>
+							</div>
+						))}
+					</div>
+				</fieldset>
+				<div className="px-4 py-3 mt-6 text-right bg-gray-100 sm:px-6">
+					<button
+						type="submit"
+						className="inline-flex justify-center px-10 py-2 text-xl font-semibold text-white rounded-md shadow-sm bg-masifunde hover:bg-masifunde focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-masifunde"
+					>
+						Save
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 export default MediaForm;
