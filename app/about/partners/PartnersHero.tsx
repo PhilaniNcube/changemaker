0f539f03817ff495a6e1a82d2d8f887946a678f1
Philani Ncube <ncbphi001@gmail.com>
2024-06-30 17:59:45 +0200

    refactor: Add lazy loading for carousel images

--- app/about/partners/PartnersHero.tsx
+++ app/about/partners/PartnersHero.tsx
@@ -1,23 +1,110 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
+const carouselImages = [
+	{
+		src: "/images/carousel/img-1.jpg",
+	},
+	{
+		src: "/images/carousel/img-2.jpg",
+	},
+	{
+		src: "/images/carousel/img-3.jpg",
+	},
+	{
+		src: "/images/carousel/img-4.jpg",
+	},
+];
 
 const PartnersHero = () => {
-  return (
-    <section id="partners" className="relative py-8 bg-masifunde isolate">
-      <Image
-        src="/images/cso.jpg"
-        width={2000}
-        height={1600}
-        alt="Team"
-        className="w-full min-h-[55vh] aspect-video object-cover"
-      />
-      <div className="absolute inset-0 py-10 bg-slate-600/60 ">
-        <div className="flex flex-col items-start justify-center h-full gap-8 px-8 mx-auto text-white max-w-7xl">
-          <h2 className="font-extrabold text-3xl lg:text-5xl max-w-[32ch] text-center mx-auto">
-            Partner CSO&apos;s Profiles
-          </h2>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section id="partners" className="relative bg-masifunde isolate">
+			<div className="grid grid-cols-2 lg:grid-cols-5">
+				<div className="flex flex-col items-center justify-center w-full lg:col-span-2">
+					<div className="px-12 md:px-20 lg:px-24">
+						<h1 className="text-4xl font-bold text-white">
+							Partner CSO Profiles
+						</h1>
+						<p className="font-medium text-white text-md lg:text-lg">
+							Learn more below about our partner Civil Society Organisations
+						</p>
+					</div>
+				</div>
+				<div className="col-span-1 lg:col-span-3 bg-masifunde">
+					<Carousel
+						opts={{
+							align: "start",
+							loop: true,
+						}}
+						plugins={[
+							Autoplay({
+								delay: 2000,
+							}),
+						]}
+						className="w-full p-0"
+					>
+						<CarouselContent className="p-0">
+							<CarouselItem className="p-0">
+								<CardContent className="flex items-center justify-center h-full p-0">
+									<Image
+										src="/images/carousel/img-1.jpg"
+										alt="carousel"
+										width={1600}
+										height={1200}
+										className="object-cover w-full aspect-[5/3] object-top"
+									/>
+								</CardContent>
+							</CarouselItem>
+							<CarouselItem className="p-0">
+								<CardContent className="flex items-center justify-center h-full p-0">
+									<Image
+										src="/images/carousel/img-2.jpg"
+										alt="carousel"
+										width={1600}
+										height={1200}
+										className="object-cover w-full aspect-[5/3] object-top"
+									/>
+								</CardContent>
+							</CarouselItem>
+							<CarouselItem className="p-0">
+								<CardContent className="flex items-center justify-center h-full p-0">
+									<Image
+										src="/images/carousel/img-3.jpg"
+										alt="carousel"
+										width={1600}
+										height={1200}
+										className="object-cover w-full aspect-[5/3] object-top"
+									/>
+								</CardContent>
+							</CarouselItem>
+							<CarouselItem className="p-0">
+								<CardContent className="flex items-center justify-center h-full p-0">
+									<Image
+										src="/images/carousel/img-4.jpg"
+										alt="carousel"
+										width={1600}
+										height={1200}
+										className="object-cover w-full aspect-[5/3] object-top"
+									/>
+								</CardContent>
+							</CarouselItem>
+						</CarouselContent>
+						<CarouselPrevious className="absolute transform translate-y-1/2 left-4 top-1/2" />
+						<CarouselNext className="absolute transform translate-y-1/2 right-4 top-1/2" />
+					</Carousel>
+				</div>
+			</div>
+		</section>
+	);
 };
 export default PartnersHero;
