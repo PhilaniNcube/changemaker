628cd94931e96fa5a202e1e9a00406e34682d10d
Philani Ncube <ncbphi001@gmail.com>
2024-07-30 22:38:52 +0200

    refactor: Update Footer component with improved layout and social media links

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { Facebook, Instagram } from 'lucide-react';
 import Image from 'next/image'
 import Link from 'next/link';
 import React from 'react'
@@ -31,29 +32,52 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-black py-20">
-      <div className="max-w-7xl mx-auto px-8">
-        <div className="flex items-center justify-between gap-3">
-          <Image
-            src="/images/logo_2.svg"
-            width={297}
-            height={167}
-            alt="Logo"
-            className="w-36 md:w-44 object-cover"
-          />
+			<footer className="py-20 bg-black">
+				<div className="px-8 mx-auto max-w-7xl">
+					<div className="flex items-center justify-between gap-3">
+						<Image
+							src="/images/logo_2.svg"
+							width={297}
+							height={167}
+							alt="Logo"
+							className="object-cover w-36 md:w-44"
+						/>
 
-          <div>
-            <p className="text-white text-md lg:text-lg font-bold">masifunde Changemaker Network &copy;</p>
-            <div className="flex flex-col items-start">
-              {links.map((link) => (
-                <Link key={link.title} href={link.href} className="text-xs lg:text-sm text-white space-y-2 hover:text-masifunde font-medium">{link.title}</Link>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+						<div>
+							<p className="font-bold text-white text-md lg:text-lg">
+								masifunde Changemaker Network &copy;
+							</p>
+							<div className="flex flex-col items-start">
+								{links.map((link) => (
+									<Link
+										key={link.title}
+										href={link.href}
+										className="space-y-2 text-xs font-medium text-white lg:text-sm hover:text-masifunde"
+									>
+										{link.title}
+									</Link>
+								))}
+								<Link
+									href="https://www.facebook.com/profile.php?id=100083194785525"
+									prefetch={false}
+									className="pb-1 space-y-2 text-xs font-medium text-white lg:text-sm hover:text-masifunde"
+								>
+									<Facebook size={20} className="inline-block mr-2" /> Facebook
+								</Link>
+								<Link
+									href="https://www.instagram.com/changemaker_network/"
+									prefetch={false}
+									className="space-y-2 text-xs font-medium text-white lg:text-sm hover:text-masifunde"
+								>
+									<Instagram size={20} className="inline-block mr-2" />{" "}
+									Instagram
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		);
 }
 
 export default Footer
