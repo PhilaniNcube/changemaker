5d80c662e012546bd9a1f395acb0bafd1e40a405
Philani Ncube <ncbphi001@gmail.com>
2025-06-12 19:19:23 +0200

    Refactor: Simplify profile data fetching and include documents in AccountPage

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -3,49 +3,35 @@ import createClient from "../../../lib/supabase-server";
 import { getOrganisations } from "@/fetchers/organisations";
 import CreateFolder from "./CreateFolder";
 import AccountPage from "@/app/account/[id]/AccountPage";
-
-
-const getProfile = async () => {
-   const supabase = createClient();
-
-
-
-   const {
-     data: { user },
-   } = await supabase.auth.getUser();
-
-   if(!user) {
-      return null
-   }
-
-   const {data:profile, error} = await supabase.from('profiles').select('*, organisation_id(*)').eq('id', user?.id).single()
-
-   if(error || !profile) {
-    return null
-   }
-
-   return profile
-}
-
+import { getDocuments } from "@/fetchers/documents";
+import { getMyProfile, getProfile } from "@/fetchers/profiles";
 
 const page = async () => {
+  const profileData = getMyProfile();
+  const organisationsData = getOrganisations();
+  const documentsData = getDocuments();
 
-  const profileData =  getProfile()
-  const organisationsData =  getOrganisations()
-
-  const [profile, organisations] = await Promise.all([
+  const [profile, organisations, documents] = await Promise.all([
     profileData,
     organisationsData,
+    documentsData,
   ]);
 
-  return <main>
-
-    <div className="flex flex-col gap-8">
-   {profile ? <AccountPage profile={profile} organisations={organisations} /> : null}
-    <div className="w-full mt-3 md:w-2/3 lg:w-1/2">
-    <CreateFolder />
-    </div>
-    </div>
-  </main>;
+  return (
+    <main>
+      <div className="flex flex-col gap-8">
+        {profile ? (
+          <AccountPage
+            profile={profile}
+            organisations={organisations}
+            documents={documents}
+          />
+        ) : null}
+        <div className="w-full mt-3 md:w-2/3 lg:w-1/2">
+          <CreateFolder />
+        </div>
+      </div>
+    </main>
+  );
 };
 export default page;
